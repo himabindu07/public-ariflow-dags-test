0b2d19012d031777666f3f1d0ee22ca247100a1f
himabindu <47584863+himabindu07@users.noreply.github.com>
2025-12-02 22:13:53 -0800

    adding af3 dags

---
+++ dags-af2/trigger_dagrun_controller.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+#dffff
+# test webhook ttt
+"""This example illustrates the use of the TriggerDagRunOperator. There are 2
+entities at work in this scenario:
+1. The Controller DAG - the DAG that conditionally executes the trigger
+2. The Target DAG - DAG being triggered (in trigger_dagrun_target.py)
+
+"""
+
+dag = DAG(
+    dag_id="trigger_controller_dag",
+    default_args={"owner": "airflow", "start_date": days_ago(2)},
+    schedule_interval=None,
+    tags=["core"],
+)
+
+
+trigger = TriggerDagRunOperator(
+    task_id="test_trigger_dagrun",
+    trigger_dag_id="trigger_target_dag",
+    reset_dag_run=True,
+    wait_for_completion=True,
+    conf={"message": "Hello World"},
+    dag=dag,
+)
---
+++ dags-af2/trigger_dagrun_target.py
@@ -0,0 +1,34 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+# test webhook change 
+dag = DAG(
+    dag_id="trigger_target_dag",
+    default_args={"start_date": days_ago(2), "owner": "Airflow"},
+    tags=["core"],
+    schedule_interval=None,  # This must be none so it's triggered by the controller
+    is_paused_upon_creation=False, # This must be set so other workers can pick this dag up. mabye it's a bug idk
+)
+
+
+def run_this_func(**context):
+    print(
+        f"Remotely received value of {context['dag_run'].conf['message']} for key=message "
+    )
+
+
+run_this = PythonOperator(
+    task_id="run_this",
+    python_callable=run_this_func,
+    dag=dag,
+)
+
+# You can also access the DagRun object in templates
+bash_task = BashOperator(
+    task_id="bash_task",
+    bash_command='echo "Here is the message: $message"',
+    env={"message": '{{ dag_run.conf["message"] if dag_run else "" }}'},
+    dag=dag,
+)
